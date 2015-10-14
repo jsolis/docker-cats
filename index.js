dba5d78b3b1122fd80ed7234a7657332913001cd
Jason Solis <jsolis@gmail.com>
2015-10-14 08:13:56 -0400

    fixing content

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Priceline.com</h1>\n');
+  res.write('<h2>Find cat friendly Hotels</h2>\n');
   res.end();
 }).listen(3000);
 
-console.log('Server running at http://127.0.0.1:3000/');
+console.log('Server running at http://0.0.0.0:3000/');
